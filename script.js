4581acc5f4294e2a56f4f533d58da44f32a1de1f
Rocktx <128630951+Rocktx@users.noreply.github.com>
2023-08-29 17:56:36 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,62 @@
+// function alterarNome() {
+
+//     let nomeDaVariavel = 'Thales Hipolito'
+//     console.log(nomeDaVariavel)
+//     nomeDaVariavel = 'Thales Viegas'
+//     console.log(nomeDaVariavel) 
+    
+// } 
+// alterarNome()
+
+// let largura = 150
+// let nomeMedida = 'cm'
+
+// let medidaTotal = `A medida total é de: ${largura} e o tipo da medida é: ${nomeMedida}`
+
+// console.log(medidaTotal)
+
+// let nomeDaSuaVariavel = 'Thales'
+// let templateString = `Abrir crase, isso para nome de elementos js ->${nomeDaSuaVariavel}`
+
+// function exercicio() {
+
+//     let nomeDoUsuario = 'Roque'
+//     let faculdadeDoUsuario = 'Unipê'
+//     let cursoDoUsuario = 'SPI'
+
+//     let templateString = `Olá me chamo ${nomeDoUsuario} , estou fazendo faculdade na ${faculdadeDoUsuario} é estou no P2 de ${cursoDoUsuario}`
+
+//     console.log(templateString)
+// }
+// exercicio()
+
+// function frutas(){
+//     const quero = document.getElementById('abacaxi')
+//     console.log(quero)
+// }
+// frutas()
+
+// function criarElemento() {
+//     const divpai1 = document.getElementById('meuContent2')
+
+//     const title = document.createElement('h1')
+//     title innerText = 'Roque'
+//     divpai1.appendChild(title)
+    
+//     const subTitle = document.createElement('p')
+//     subTitle innerText = 'Unipê'
+//     divpai1.appendChild(title)
+// }
+
+// const meuArray = ['Henrique', 'Roque']
+// const meuObjeto = {
+//     nomeCompleto : 'Henrique Roque',
+//     faculdade : 'Unipe',
+//     periodo : '2 Periodo'
+
+// }
+//     console.log(meuObjeto)
+
+// const meuArray = ['Thales', ]
+// for(i = 0; i <= 2;i++)
+// console.log(i)
